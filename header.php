e25ba2aa2445006fdf280a26d146bc5a2163281e
Islay Anderson <mranderson64@live.com>
2023-11-07 00:19:13 +0000

    add google analytics

--- header.php
+++ header.php
@@ -39,7 +39,15 @@
 		<?php // end analytics ?>
 
 	</head>
-
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-823JYN3Q13"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'G-823JYN3Q13');
+	</script>
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
 		<div id="container">
