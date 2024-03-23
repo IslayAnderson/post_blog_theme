796b4c84cfa035826421e09148284f2e07da0074
Islay Anderson <islayanderson@outlook.com>
2024-03-23 01:08:54 +0000

    move sass and chnage column layout

--- index.php
+++ index.php
@@ -10,32 +10,10 @@
                             ?>
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                                 <?php
-                                $thumbnail = !empty(get_the_post_thumbnail_url(get_the_ID(),'full'))?"<img src=\"".get_the_post_thumbnail_url(get_the_ID(),'full')."\">": "<img src=\"".get_template_directory_uri() . "/" . explode("/poster_blog/",$thumbail_rand[rand(0, count($thumbail_rand)-1)])[1]."\">";
-                                    if($counter%2):
+                                $thumbnail = !empty(get_the_post_thumbnail_url(get_the_ID(),'full'))?"<img src=\"".get_the_post_thumbnail_url(get_the_ID(),'full')."\">": "";
                                 ?>
-                                    <div class=" project headline"  id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-                                        <a class="section_headline" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><h2 class="title"><?php the_title(); ?></h2></a>
-                                        <div class="h_line"></div>
-                                        <p class="byline entry-meta vcard">
-                                            <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                                                /* the time the post was published */
-                                                '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                                                /* the author of the post */
-                                                '<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                                            ); ?>
-                                        </p>
-
-                                        <?php echo TruncateHTML::truncateWords(get_the_content(), '150', '...'); ?>
-                                    </div>
-                                    <figure class="project proof">
-                                        <?=$thumbnail ?>
-                                    </figure>
-                                <?php else: ?>
-                                    <figure class="project proof">
-                                        <?=$thumbnail?>
-                                    </figure>
                                     <div class="article-header project headline"  id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-                                        <a class="section_headline" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><h2 class="title"><?php the_title(); ?></h2></a>
+                                        <a class="section_headline" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><h2 class="title"><?php the_title(); ?></h2></a> 
                                         <div class="h_line"></div>
                                         <p class="byline entry-meta vcard">
                                             <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
@@ -45,11 +23,13 @@
                                                 '<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
                                             ); ?>
                                         </p>
-
-                                        <?php echo TruncateHTML::truncateWords(get_the_content(), '150', '...'); ?>
+                                        <figure class="project proof">
+                                            <?=$thumbnail?>
+                                        </figure>
+                                        <?php //echo TruncateHTML::truncateWords(get_the_content(), '150', '...'); ?>
+                                        <?php echo the_excerpt(); ?>
                                     </div>
-                                <?php endif;?>
-							<?php $counter++; endwhile; ?>
+							<?php  endwhile; ?>
 
 									<?php bones_page_navi(); ?>
 
@@ -57,13 +37,13 @@
 
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+												<h1>Post not found!</h1>
 										</header>
 											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+												<p>It's brokey</p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+												
 										</footer>
 									</article>
 
