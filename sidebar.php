6dda48882585a145065d84d725dfb0d66f9d1f9a
Islay Anderson <islayanderson@outlook.com>
2024-07-13 19:40:40 +0100

    GDS archive

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,15 @@
-				<div id="sidebar1" role="complementary">
+<div id="sidebar1" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
+		<?php dynamic_sidebar( 'sidebar1' ); ?>
 
-					<?php else : ?>
+	<?php else : ?>
 
-						<div class="no-widgets">
-							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'gdstheme' );  ?></p>
-						</div>
+		<div class="no-widgets">
+			<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'gdstheme' );  ?></p>
+		</div>
 
-					<?php endif; ?>
+	<?php endif; ?>
 
-				</div>
+</div>
