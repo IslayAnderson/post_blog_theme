ce4a3fe8190c76c0ba1029de59c6194ed294552b
Islay Anderson <islayanderson@outlook.com>
2024-07-13 20:14:26 +0100

    updated search form layout and commented unusedcss from gulp

--- searchform.php
+++ searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div class="govuk-form-group">
-        <label class="govuk-label" for="s"><?php _e('Search for:','gdstheme'); ?></label>
-        <input class="govuk-input" type="search" id="s" name="s" value="" />
-        <button class="govuk-button govuk-button--secondary" data-module="govuk-button" type="submit" id="searchsubmit" ><?php _e('Search','gdstheme'); ?></button>
-    </div>
+            <label class="govuk-label" for="s"><?php _e('Search for:','gdstheme'); ?></label>
+        <input class="govuk-input govuk-!-width-two-thirds" type="search" id="s" name="s" value="" />
+        <button class="govuk-button govuk-button--secondary govuk-!-static-margin-bottom-0" data-module="govuk-button" type="submit" id="searchsubmit" ><?php _e('Search','gdstheme'); ?></button>
 </form>
